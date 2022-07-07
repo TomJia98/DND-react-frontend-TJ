1e065d927004e7f2fb3cd3e296433f9d62983d71
tomjia98 <tomchristian1998@gmail.com>
2022-07-07 18:27:20 +0800

    added some functionality for a page selector on the spells.js

--- dnd-reactfrontend-tj/src/pages/spells.js
+++ dnd-reactfrontend-tj/src/pages/spells.js
@@ -2,36 +2,82 @@ import React, { useState } from "react";
 import { getSpells } from "../components/dnd-api";
 import Select from "react-select";
 
-let options = [];
-const spellsData = async () => {
-  const results = await getSpells();
-  if (options.length === 0) {
-    //stops the options from doubling up on re-render
-    const resultsArr = results.results;
-    for (let i = 0; i < resultsArr.length; i++) {
-      const el = { value: resultsArr[i].index, label: resultsArr[i].name };
-      options.push(el);
-    }
-  }
-
-  console.log(results.results);
-};
-
 const Spells = () => {
+  let options = [];
+
+  const [page, setPage] = useState(1);
+  const [pageSettings, setPageSettings] = useState(options, page, 20);
   const [selectedSpell, setSelectedSpell] = useState("");
+
+  const spellsData = async () => {
+    const results = await getSpells();
+    if (options.length === 0) {
+      //stops the options from doubling up on re-render
+      const resultsArr = results.results;
+      for (let i = 0; i < resultsArr.length; i++) {
+        const el = { value: resultsArr[i].index, label: resultsArr[i].name };
+        options.push(el);
+      }
+    }
+  };
+  console.log();
   spellsData();
+
+  const increasePage = () => {
+    setPage(page + 1);
+  };
+
+  const decreasePage = () => {
+    setPage(page - 1);
+  };
+
+  const pageDisplay = () => {
+    if (page != 1) {
+      return (
+        <div>
+          <button onClick={decreasePage}>-</button>
+          <span>{page}</span>
+          <button onClick={increasePage}>+</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <span>{page}</span>
+          <button onClick={increasePage}>-</button>
+        </div>
+      );
+    }
+  };
+
+  const pagesFunct = (array, page, amount = 20) => {
+    //lets the user select a page of results,as well as how many results are on each page
+    const min = page * amount;
+    const returnArr = [];
+    for (let i = min; i < min + amount; i++) {
+      returnArr.push(array[i]);
+    }
+    return returnArr;
+  };
   const handleSpellSelection = (ev) => {
     setSelectedSpell(ev.label);
   };
   return (
     <div>
-      <Select
-        isMulti={false}
-        options={options}
-        closeMenuOnSelect={true}
-        onChange={handleSpellSelection}
-      />
+      <div className="selectDropdown">
+        <p>Know what your looking for? Search for it here</p>
+        <Select
+          isMulti={false}
+          options={options}
+          closeMenuOnSelect={true}
+          onChange={handleSpellSelection}
+        />
+      </div>
       <p>{selectedSpell}</p>
+      <div className="alphabetSection">
+        {}
+        {pageDisplay()}
+      </div>
     </div>
   );
 };
