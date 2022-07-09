3e9f2a5df28f46b10508c1adadd639ee22669828
tomjia98 <tomchristian1998@gmail.com>
2022-07-09 13:58:19 +0800

    page now stays between max and min amounts

--- dnd-reactfrontend-tj/src/pages/spells.js
+++ dnd-reactfrontend-tj/src/pages/spells.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { getSpells, getSpellInfo } from "../components/dnd-api";
 import Select from "react-select";
 
+let lastPage = false;
 const Spells = () => {
   let [options, setOptions] = useState();
   const [page, setPage] = useState(1);
@@ -30,27 +31,6 @@ const Spells = () => {
     setPage(page - 1);
   };
 
-  const pageDisplay = () => {
-    if (page !== 1) {
-      return (
-        <span id="pageButtons">
-          <button onClick={decreasePage}>&#8592;</button>
-          <span>{page}</span>
-          <button onClick={increasePage}>&#8594;</button>
-        </span>
-      );
-    } else {
-      return (
-        <span id="pageButtons">
-          <button onClick={decreasePage} disabled>
-            &#8592;
-          </button>
-          <span>{page}</span>
-          <button onClick={increasePage}>&#8594;</button>
-        </span>
-      );
-    }
-  };
   const SetActiveSpellDropdown = async (e) => {
     const resp = await getSpellInfo(e.value);
     setSpellInfo(resp);
@@ -64,18 +44,57 @@ const Spells = () => {
 
     const min = page * amount - 20;
     const returnArr = [];
+    let enteredLast = false;
     let max = min + amount;
     if (array.length < max) {
       max = array.length;
+      lastPage = true;
+      enteredLast = true;
     }
     for (let i = min; i < max; i++) {
       if (array[i] !== undefined) {
         returnArr.push(array[i]);
-      } else return;
+      } else {
+        return;
+      }
+    }
+    if (!enteredLast) {
+      lastPage = false;
     }
     return returnArr;
   };
-
+  const pageDisplay = () => {
+    if (lastPage === true) {
+      return (
+        <span id="pageButtons">
+          <button onClick={decreasePage}>&#8592;</button>
+          <span>{page}</span>
+          <button onClick={increasePage} disabled>
+            &#8594;
+          </button>
+        </span>
+      );
+    } else {
+      if (page !== 1) {
+        return (
+          <span id="pageButtons">
+            <button onClick={decreasePage}>&#8592;</button>
+            <span>{page}</span>
+            <button onClick={increasePage}>&#8594;</button>
+          </span>
+        );
+      }
+      return (
+        <span id="pageButtons">
+          <button onClick={decreasePage} disabled>
+            &#8592;
+          </button>
+          <span>{page}</span>
+          <button onClick={increasePage}>&#8594;</button>
+        </span>
+      );
+    }
+  };
   return (
     <div>
       <div className="selectDropdown">
@@ -87,6 +106,7 @@ const Spells = () => {
           onChange={SetActiveSpellDropdown}
         />
       </div>
+      {pageDisplay()}
       <div id="infoSection">
         {options ? (
           <>
@@ -101,7 +121,6 @@ const Spells = () => {
                 } else return;
               })}
             </ul>
-            {pageDisplay()}
             <span id="spellInfo">
               {spellInfo ? (
                 <>
@@ -131,3 +150,5 @@ const Spells = () => {
 export default Spells;
 //need to add a good looking way to display all the ~300 spells, add a search function with autofill. Each element needs to have a dropdown with the info for the spell on hover,
 //as well as a fav button. Fav button should save the spell to localStorage for the fav page to pull and display
+
+//change the page
