f8651e833308dc212e3b66825791c6ca4426a39a
tomjia98 <tomchristian1998@gmail.com>
2022-07-10 12:11:41 +0800

    added moving background

--- dnd-reactfrontend-tj/src/App.js
+++ dnd-reactfrontend-tj/src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Spells from "./pages/spells";
 import Favourites from "./pages/favourites";
+import spellBackground from "./images/spellBackground.jpg";
 
 function App() {
+  let [xOffset, setX] = useState();
+  let [yOffset, setY] = useState();
+
+  const mouseMove = (e) => {
+    setX(e.clientX + -3500);
+    setY(e.clientY + -2000);
+  };
   return (
     <Router>
-      <div className="App">
+      <div
+        className="App"
+        onMouseMove={mouseMove}
+        style={{
+          background:
+            "url(/static/media/spellBackground.7769a55d7dbe4c6840ac.jpg)",
+          backgroundPositionX: xOffset / 4 + "px",
+          backgroundPositionY: yOffset / 5 + "px",
+          height: "110vh",
+        }}
+      >
         <header>
           <h1>DND Spell Saver</h1>
         </header>
